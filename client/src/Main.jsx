0f9f26ba78219da456cdcf3222c6709bb15928fb
PathFOSS <fossprogrammingprojects@proton.me>
2024-03-12 18:14:10 +0200

    Created React router

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { Store } from './redux/Store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { Store } from './redux/Store.js';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './components/NotFound.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound/>
+    },
+    {
+        path: "/currency",
+        element: <App/>
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={Store}>
-        <App/>
+        <RouterProvider router={router}/>
     </Provider>
 )
---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,4 @@
+const NotFound = () => {
+    return <h1>Page not found</h1>
+}
+export default NotFound;
