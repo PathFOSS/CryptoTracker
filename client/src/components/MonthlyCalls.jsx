104ba5eedb3f6c348f546206fe77b11d7511b751
PathFOSS <fossprogrammingprojects@proton.me>
2024-03-15 13:45:20 +0200

    Added additional text near banner

--- client/src/components/MonthlyCalls.jsx
+++ client/src/components/MonthlyCalls.jsx
@@ -2,6 +2,6 @@ import { useSelector } from "react-redux";
 
 const MonthlyCalls = () => {
     const callsTotal = useSelector((state) => state.calls.value);
-    return <h5>Total API calls this month: {callsTotal}</h5>
+    return <h5>Total API calls this month: {callsTotal} / 10000</h5>
 }
 export default MonthlyCalls;
