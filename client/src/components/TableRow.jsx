943d7a4cf2d8038bd36572343727734f91062421
PathFOSS <email@email.com>
2024-04-16 10:45:56 +0300

    Updated loading UX

--- client/src/components/TableRow.jsx
+++ client/src/components/TableRow.jsx
@@ -10,12 +10,12 @@ const TableRow = (props) => {
     return <tr onClick={() => dispatch(changeCoin(props.ticker))}>
         <td className="rank">{props.rank}</td>
         <td className="name">{props.name} <span className="ticker">{props.ticker}</span></td>
-        <td>${props.priceData ? SimplifyNumber(props.priceData.price, 2) : null}</td>
-        <td className="colorized-cell hide-data"><PriceChange price={props.priceData ? props.priceData.percent_change_1h : null}/></td>
-        <td className="colorized-cell"><PriceChange price={props.priceData ? props.priceData.percent_change_24h : null}/></td>
-        <td className="colorized-cell hide-data"><PriceChange price={props.priceData ? props.priceData.percent_change_7d : null}/></td>
-        <td className="hide-data">${props.priceData ? SimplifyNumber(props.priceData.market_cap, 0) : null}</td>
-        <td className="hide-data">${props.priceData ? SimplifyNumber(props.priceData.volume_24h, 0) : null}</td>
+        <td>{props.priceData ? `$${SimplifyNumber(props.priceData.price, 2)}` : null}</td>
+        <td className="colorized-cell hide-data">{props.priceData ? <PriceChange price={props.priceData.percent_change_1h}/> : null}</td>
+        <td className="colorized-cell">{props.priceData ? <PriceChange price={props.priceData.percent_change_24h}/> : null}</td>
+        <td className="colorized-cell hide-data">{props.priceData ? <PriceChange price={props.priceData.percent_change_7d}/> : null}</td>
+        <td className="hide-data">{props.priceData ? `$${SimplifyNumber(props.priceData.market_cap, 0)}` : null}</td>
+        <td className="hide-data">{props.priceData ? `$${SimplifyNumber(props.priceData.volume_24h, 0)}` : null}</td>
         <td className="hide-data">{props.priceData ? SimplifyNumber(props.circulatingSupply, 0) : null} {props.ticker}</td>
     </tr>
 }
