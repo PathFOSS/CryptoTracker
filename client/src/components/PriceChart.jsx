369f83622c5ed3892a5a2d1b2358b162f294d74e
PathFOSS <email@email.com>
2024-05-23 13:42:30 +0300

    Added tooltip to charts

--- client/src/components/PriceChart.jsx
+++ client/src/components/PriceChart.jsx
@@ -1,7 +1,8 @@
-import { AreaChart, Area, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from "recharts";
+import { AreaChart, Area, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 import { SecondaryWhite, BrandColor } from "../data/Colors";
 import FormatTick from "../functions/FormatTick";
 import FormatDate from "../functions/FormatDate";
+import ModifyNumber from "../functions/ModifyNumber";
 
 const PriceChart = (props) => {
 
@@ -12,13 +13,21 @@ const PriceChart = (props) => {
         {label: 0, price: props.priceData.price}
     ];
 
+    const CustomToolTip = ({ payload, label, active}) => {
+        if (active) {
+            const value = "$" + ModifyNumber(payload[0].value, 2);
+            return <div>{value}</div>
+        }
+    };
+
     return <div id="chart-container">
         <ResponsiveContainer>
             <AreaChart width={500} height={400} data={priceData}>
                 <YAxis type="number" width={props.priceData.price < 0.00001 || props.priceData.price >= 100000 ? 60 : 40} tick={{ fill: SecondaryWhite }} tickCount={10} interval={0} fontSize={"0.75rem"} orientation="left" domain={["auto", "auto"]} tickFormatter={FormatTick}/>
-                <XAxis dataKey="label" tick={{ fill: SecondaryWhite }} fontSize={"0.75rem"} tickFormatter={FormatDate}/>
+                <XAxis dataKey="label" tick={{ fill: SecondaryWhite }} fontSize={"0.75rem"} tickFormatter={FormatDate} tickMargin={10}/>
                 <CartesianGrid stroke={SecondaryWhite} strokeDasharray="4 4"/>
                 <Area type="monotone" dataKey="price" fill={SecondaryWhite} stroke={BrandColor} strokeWidth={2}/>
+                <Tooltip content={<CustomToolTip/>}/>
             </AreaChart>
         </ResponsiveContainer>
     </div>
